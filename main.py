d01eb75c00edd8320eb88fb2fa0bf6b0ed9a0ae1
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-01-23 10:53:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ from math import sqrt
 
 def  someDoMonteCarlo(n):
     circle = 0
-
+    
     for i in range(1, n + 1):
+        
         x = random.random()
         y = random.random()
-
+        
         # Checking to see if the produced number falls into the circle
         if sqrt(x ** 2 + y ** 2) <= 1:
             circle += 1
-
+            
     Pi = 4 * (circle / n)
-
-
     return Pi
 
 
 
 n = [10, 100, 1000]
-
 Pi_storage = {}
 
 for j in n:
